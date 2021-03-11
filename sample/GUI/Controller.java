677d0bdcb9eedf485a1d4e4b19ec0fe85997393c
Сергей <sergey_svotin@mail.ru>
2021-03-12 01:07:06 +0400

    Alpha 1.0 build 1.047623

--- sample/GUI/Controller.java
+++ sample/GUI/Controller.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import sample.BackEnd.Squads.Squad;
-import sample.BackEnd.Squads.Units.Unit;
 import sample.Service.Battle;
 import java.util.regex.Pattern;
 
@@ -39,17 +38,17 @@ public class Controller {
         else {
             Battle battle = new Battle(getFirstSquadName(), getSecondSquadName());
             outputWindow.setText(battle.getDateHelper().getFormattedStartDate() +
-                    "\nБитва началась!\n\n");
+                    "\nБитва началась!\n");
             while (battle.getRedSquad().hasAliveUnits() && battle.getBlueSquad().hasAliveUnits()) {
-                provideBattleLog(battle, battle.makeAttack(battle.getRedSquad(),
-                        battle.getBlueSquad()), battle.getBlueSquad());
+                for(String string: battle.makeAttack(battle.getRedSquad(), battle.getBlueSquad()))
+                    outputWindow.appendText(string);
                 if (battle.getBlueSquad().hasAliveUnits())
-                    provideBattleLog(battle, battle.makeAttack(battle.getBlueSquad(),
-                            battle.getRedSquad()), battle.getRedSquad());
+                    for(String string: battle.makeAttack(battle.getBlueSquad(), battle.getRedSquad()))
+                        outputWindow.appendText(string);
             }
             if (battle.getRedSquad().hasAliveUnits())
-                outputWindow.appendText(getResult(battle.getRedSquad()));
-            else outputWindow.appendText(getResult(battle.getBlueSquad()));
+                outputWindow.appendText("\nПобедил " + battle.getRedSquad().toString() + " отряд.");
+            else outputWindow.appendText("\nПобедил " + battle.getBlueSquad().toString() + " отряд.");
             outputWindow.appendText("\n" + "Битва продолжалась " +
                     battle.getDateHelper().getFormattedDiff());
         }
@@ -67,55 +66,5 @@ public class Controller {
         Pattern pattern = Pattern.compile("[a-zA-Zа-яА-Я0-9]+");
         return (!pattern.matcher(firstSquadName).matches() ||
                 !pattern.matcher(secondSquadName).matches());
-    }
-
-    private static String getResult(Squad squad) {
-        return "Победил " + squad.toString() + " отряд.";
-    }
-
-    private void provideBattleLog(Battle battle, Object[] data, Squad squad) {
-        outputWindow.appendText(battle.getDateHelper().getDate());
-        Unit unit1 = (Unit) data[0];
-        Unit unit2 = (Unit) data[1];
-        int attack = (Integer) data[2];
-        int vitality = (Integer) data[4];
-        int damage = (Integer) data[5];
-        outputWindow.appendText("\n" + unit1 + unit1.getUnitVitalityCard() + " атакует " + unit2 +
-                unit2.getUnitVitalityCard(vitality));
-        outputWindow.appendText("\n" + unit1 + " наносит " + attack + "(-" + (attack - damage) +
-                ")" + reformString(attack) + " урона.");
-        if ((Integer) data[3] == 1)
-            outputWindow.appendText(" Критический удар!");
-        if (unit2.isAlive())
-            outputWindow.appendText("\n" + unit2 + " теряет " + damage + reformString(damage) +
-                    " здоровья.\n\n");
-        else outputWindow.appendText("\n" + unit2 + " теряет " + vitality + reformString(vitality) +
-                " здоровья.\n");
-        if (!unit2.isAlive())
-            outputWindow.appendText("Боец убит.\n\n");
-        if (squad.hasAliveUnits()) battle.getDateHelper().skipTime();
-    }
-
-    private String reformString(int input) {
-        String output;
-        if (input % 100 > 20 || input % 100 < 10) {
-            switch (input % 10) {
-                case 1: {
-                    output = " единицу";
-                    break;
-                }
-                case 2:
-                case 3:
-                case 4: {
-                    output = " единицы";
-                    break;
-                }
-                default: {
-                    output = " единиц";
-                    break;
-                }
-            }
-        } else output = " единиц";
-        return output;
     }
 }
--- sample/Service/Battle.java
+++ sample/Service/Battle.java
@@ -4,6 +4,8 @@ import sample.BackEnd.DateHelper;
 import sample.BackEnd.Squads.Squad;
 import sample.BackEnd.Squads.Units.Unit;
 
+import java.util.ArrayList;
+
 public class Battle {
     private final DateHelper dateHelper;
     private final Squad redSquad;
@@ -15,13 +17,25 @@ public class Battle {
         this.blueSquad = new Squad(secondName);
     }
 
-    public Object[] makeAttack(Squad squad1, Squad squad2){
+    public ArrayList<String> makeAttack(Squad squad1, Squad squad2) {
+        ArrayList<String> attackLog = new ArrayList<>();
+        attackLog.add("\n" + this.dateHelper.getDate());
         Unit unit1 = squad1.getRandomUnit();
         Unit unit2 = squad2.getRandomUnit();
+        attackLog.add("\n" + unit1 + unit1.getUnitVitalityCard() + " атакует " + unit2 + unit2.getUnitVitalityCard());
         int[] attackData = unit1.attack();
         int vitality = unit2.getCurrentVitality();
         int damage = unit2.takeDamage(unit1, attackData[0]);
-        return new Object[]{unit1, unit2, attackData[0], attackData[1], vitality, damage};
+        attackLog.add("\n" + unit1 + " наносит " + attackData[0] + "(-" + (attackData[0] - damage) + ")" +
+                reformString(attackData[0]) + " урона.");
+        if (attackData[1] == 1)
+            attackLog.add(" Критический удар!");
+        if (unit2.isAlive())
+            attackLog.add("\n" + unit2 + " теряет " + damage + reformString(damage) + " здоровья.\n");
+        else attackLog.add("\n" + unit2 + " теряет " + vitality + reformString(vitality) + " здоровья.\n");
+        if (!unit2.isAlive()) attackLog.add("Боец убит.\n");
+        if (squad2.hasAliveUnits()) this.dateHelper.skipTime();
+        return attackLog;
     }
 
     public Squad getRedSquad() {
@@ -34,5 +48,28 @@ public class Battle {
 
     public DateHelper getDateHelper() {
         return this.dateHelper;
+    }
+
+    private String reformString(int input) {
+        String output;
+        if (input % 100 > 20 || input % 100 < 10) {
+            switch (input % 10) {
+                case 1: {
+                    output = " единицу";
+                    break;
+                }
+                case 2:
+                case 3:
+                case 4: {
+                    output = " единицы";
+                    break;
+                }
+                default: {
+                    output = " единиц";
+                    break;
+                }
+            }
+        } else output = " единиц";
+        return output;
     }
 }
