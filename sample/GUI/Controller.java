88f1491bb3c1512f4bc2cb867306d2b62617dbec
Сергей <sergey_svotin@mail.ru>
2021-03-15 15:28:59 +0400

    Alpha 1.1 build 1.13256

--- sample/GUI/Controller.java
+++ sample/GUI/Controller.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import sample.Service.Battle;
-import java.util.regex.Pattern;
 
 public class Controller {
 
@@ -29,27 +28,15 @@ public class Controller {
     }
 
     public void makeThisShitWorks() {
-        if (getConditionOverHere(getFirstSquadName(), getSecondSquadName()))
+        if (Battle.getConditionOverHere(getFirstSquadName(), getSecondSquadName()))
             outputWindow.setText("Ошибка! Введите названия отрядов!\n" +
                     "Знаки препинания и пробелы в именах запрещены!");
         else if (getFirstSquadName().equals(getSecondSquadName()))
             outputWindow.setText("Ошибка! Имена отрядов должны различаться!");
         else {
-            Battle battle = new Battle(getFirstSquadName(), getSecondSquadName());
-            outputWindow.setText(battle.getDateHelper().getFormattedStartDate() +
-                    "\nБитва началась!\n");
-            while (battle.getRedSquad().hasAliveUnits() && battle.getBlueSquad().hasAliveUnits()) {
-                for(String string: battle.makeAttack(battle.getRedSquad(), battle.getBlueSquad()))
-                    outputWindow.appendText(string);
-                if (battle.getBlueSquad().hasAliveUnits())
-                    for(String string: battle.makeAttack(battle.getBlueSquad(), battle.getRedSquad()))
-                        outputWindow.appendText(string);
-            }
-            if (battle.getRedSquad().hasAliveUnits())
-                outputWindow.appendText("\nПобедил " + battle.getRedSquad().toString() + " отряд.");
-            else outputWindow.appendText("\nПобедил " + battle.getBlueSquad().toString() + " отряд.");
-            outputWindow.appendText("\n" + "Битва продолжалась " +
-                    battle.getDateHelper().getFormattedDiff());
+            outputWindow.clear();
+            for (String string: Battle.provideBattle(getFirstSquadName(), getSecondSquadName()))
+                outputWindow.appendText(string);
         }
     }
 
@@ -59,11 +46,5 @@ public class Controller {
 
     public void makeSecondSquadFieldBetter(){
         secondSquad.clear();
-    }
-
-    private static boolean getConditionOverHere(String firstSquadName, String secondSquadName) {
-        Pattern pattern = Pattern.compile("[a-zA-Zа-яА-Я0-9]+");
-        return (!pattern.matcher(firstSquadName).matches() ||
-                !pattern.matcher(secondSquadName).matches());
     }
 }
--- sample/Service/Battle.java
+++ sample/Service/Battle.java
@@ -3,23 +3,34 @@ package sample.Service;
 import sample.BackEnd.DateHelper;
 import sample.BackEnd.Squads.Squad;
 import sample.BackEnd.Squads.Units.Unit;
-
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class Battle {
-    private final DateHelper dateHelper;
-    private final Squad redSquad;
-    private final Squad blueSquad;
 
-    public Battle(String firstName, String secondName) {
-        this.dateHelper = new DateHelper();
-        this.redSquad = new Squad(firstName);
-        this.blueSquad = new Squad(secondName);
+    public static ArrayList<String> provideBattle(String firstSquad, String secondSquad) {
+        DateHelper dateHelper = new DateHelper();
+        Squad redSquad = new Squad(firstSquad);
+        Squad blueSquad = new Squad(secondSquad);
+        ArrayList<String> output = new ArrayList<>();
+        output.add(dateHelper.getFormattedStartDate() +
+                "\nБитва началась!\n");
+        while (redSquad.hasAliveUnits() && blueSquad.hasAliveUnits()) {
+            output.addAll(makeAttack(redSquad, blueSquad, dateHelper));
+            if (blueSquad.hasAliveUnits())
+                output.addAll(makeAttack(blueSquad, redSquad, dateHelper));
+        }
+        if (redSquad.hasAliveUnits())
+            output.add("\nПобедил " + redSquad.toString() + " отряд.");
+        else output.add("\nПобедил " + blueSquad.toString() + " отряд.");
+        output.add("\n" + "Битва продолжалась " +
+                dateHelper.getFormattedDiff());
+        return output;
     }
 
-    public ArrayList<String> makeAttack(Squad squad1, Squad squad2) {
+    private static ArrayList<String> makeAttack(Squad squad1, Squad squad2, DateHelper dateHelper) {
         ArrayList<String> attackLog = new ArrayList<>();
-        attackLog.add("\n" + this.dateHelper.getDate());
+        attackLog.add("\n" + dateHelper.getDate());
         Unit unit1 = squad1.getRandomUnit();
         Unit unit2 = squad2.getRandomUnit();
         attackLog.add("\n" + unit1 + unit1.getUnitVitalityCard() + " атакует " + unit2 + unit2.getUnitVitalityCard());
@@ -34,42 +45,25 @@ public class Battle {
             attackLog.add("\n" + unit2 + " теряет " + damage + reformString(damage) + " здоровья.\n");
         else attackLog.add("\n" + unit2 + " теряет " + vitality + reformString(vitality) + " здоровья.\n");
         if (!unit2.isAlive()) attackLog.add("Боец убит.\n");
-        if (squad2.hasAliveUnits()) this.dateHelper.skipTime();
+        if (squad2.hasAliveUnits()) dateHelper.skipTime();
         return attackLog;
     }
 
-    public Squad getRedSquad() {
-        return this.redSquad;
-    }
-
-    public Squad getBlueSquad() {
-        return this.blueSquad;
-    }
-
-    public DateHelper getDateHelper() {
-        return this.dateHelper;
-    }
-
-    private String reformString(int input) {
-        String output;
+    private static String reformString(int input) {
         if (input % 100 > 20 || input % 100 < 10) {
             switch (input % 10) {
-                case 1: {
-                    output = " единицу";
-                    break;
-                }
+                case 1: return " единицу";
                 case 2:
                 case 3:
-                case 4: {
-                    output = " единицы";
-                    break;
-                }
-                default: {
-                    output = " единиц";
-                    break;
-                }
+                case 4: return " единицы";
+                default: return " единиц";
             }
-        } else output = " единиц";
-        return output;
+        } else return " единиц";
+    }
+
+    public static boolean getConditionOverHere(String firstSquadName, String secondSquadName) {
+        Pattern pattern = Pattern.compile("[a-zA-Zа-яА-Я0-9]+");
+        return (!pattern.matcher(firstSquadName).matches() ||
+                !pattern.matcher(secondSquadName).matches());
     }
 }
